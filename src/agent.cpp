4e34ff87a87400c6443d50e90022f419c5aa4abb
yilserkabaran <yilserkabaran@gmail.com>
2015-05-27 22:19:40 +1000

    explore likes water now

--- src/agent.cpp
+++ src/agent.cpp
@@ -446,7 +446,7 @@ char World::explore(){
 		for (int i = 0; i < 4; i++) {
 			int x = current.posX + forwardX[i];
 			int y = current.posY + forwardY[i];
-			if(getMap(x,y) != ' ' && (getMap(x,y) == '~' && (getMap(current.posX,current.posY) != '~' || getMap(current.posX,current.posY) != 'B')) && getMap(x,y) != 'B'){
+			if(getMap(x,y) == '*' || getMap(x,y) == 'T'){
 				continue;
 			}
 			ExpNode nextNode(x,y,current.moves+1,this);
